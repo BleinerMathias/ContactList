8335d355b305b0d0a886bc30adad0f60027c6221
Bleiner Mathias <bleinerbbm@hotmail.com>
2023-11-14 03:32:20 -0300

    Criado activity para novo contato

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -1,9 +1,16 @@
 package br.edu.scl.ifsp.sdm.contactlist.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -19,15 +26,57 @@ class MainActivity : AppCompatActivity() {
         ArrayAdapter(this,android.R.layout.simple_list_item_1,contactList.map{it.toString()})
     }
 
+    private lateinit var contactActivityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.contact_list)
+
+        contactActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result -> if(result.resultCode === RESULT_OK){
+                val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
+                contact?.also {
+                    if(contactList.any{it.id == contact.id}){
+
+                    }else{
+                        contactList.add(contact)
+                        contactAdapter.add(contact.toString())
+                    }
+                    // Informar o adapter da modificação no datasource
+                    contactAdapter.notifyDataSetChanged()
+                }
+            }
+        }
 
         fillContacts()
 
         // Associar o listview ao adapter -> genérico
         activityMainBinding.contactsListView.adapter = contactAdapter
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            R.id.addContactMenuItem->{
+                // Apenas chama a activity
+                //startActivity(Intent(this, ContactActivity::class.java))
+
+                // Chama activity aguardando um resultado
+                contactActivityResultLauncher.launch(Intent(this, ContactActivity::class.java))
+
+                true
+            }
+            else -> {false}
+        }
     }
 
     private fun fillContacts(){
