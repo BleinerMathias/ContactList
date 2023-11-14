f247e4add73965777782a978d8a2c0a41209d171
Bleiner Mathias <bleinerbbm@hotmail.com>
2023-11-14 02:41:32 -0300

    Criando Data source e adapter genérico (string)

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -2,15 +2,46 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
-    private val amb: ActivityMainBinding by lazy {
+    private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    //Data source genérico
+    private val contactList:MutableList<Contact> = mutableListOf()
+
+    // Adapter
+    private val contactAdapter: ArrayAdapter<String> by lazy{
+        ArrayAdapter(this,android.R.layout.simple_list_item_1,contactList.map{it.toString()})
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(amb.root)
+        setContentView(activityMainBinding.root)
+
+        fillContacts()
+
+        // Associar o listview ao adapter -> genérico
+        activityMainBinding.contactsListView.adapter = contactAdapter
+
     }
+
+    private fun fillContacts(){
+        for(i in 1..50){
+            contactList.add(
+                Contact(i,
+                    "Nome $i",
+                    "Endereço $i",
+                    "Telefone $i",
+                    "Email $i"
+                )
+            )
+        }
+    }
+
+
 }
